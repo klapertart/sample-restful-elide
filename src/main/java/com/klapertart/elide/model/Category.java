4be660337b26ffb769865e59a47ce9fb9c91f801
otong.sunandar <otong.sunandar@tritronik.com>
2024-07-20 07:43:35 +0700

    enable config elide links

--- src/main/java/com/klapertart/elide/model/Category.java
+++ src/main/java/com/klapertart/elide/model/Category.java
@@ -1,12 +1,14 @@
 package com.klapertart.elide.model;
 
 import com.yahoo.elide.annotation.Include;
+import com.yahoo.elide.annotation.ReadPermission;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Include(name = "category")
+@ReadPermission(expression = "Prefab.Role.All")  // Allow read access to everyone
 public class Category{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/klapertart/elide/model/Product.java
+++ src/main/java/com/klapertart/elide/model/Product.java
@@ -1,12 +1,14 @@
 package com.klapertart.elide.model;
 
 import com.yahoo.elide.annotation.Include;
+import com.yahoo.elide.annotation.ReadPermission;
 
 import javax.persistence.*;
 
 
 @Entity
 @Include(name = "product")
+@ReadPermission(expression = "Prefab.Role.All")  // Allow read access to everyone
 public class Product {
 
     @Id
@@ -18,6 +20,6 @@ public class Product {
     private Double price;
 
     @ManyToOne
-    @JoinColumn(name = "category_id",nullable = false)
+    @JoinColumn(name = "category",nullable = false)
     private Category category;
 }
